18f76ddc8de0aad60b683da5a0b68966f8ff338e
hoangdangcodecomplete <hoang.dang@codecomplete.jp>
2022-04-29 16:37:29 +0700

    Updates

---
+++ precache-manifest.9e9a61df600441dba662d6413c58a631.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbf5cd41d9a13178f31af1fe6276f67d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0ce329b39c4c7d5e80a4",
+    "url": "./static/css/2.41a21537.chunk.css"
+  },
+  {
+    "revision": "6249a035b1e16a9a9e22",
+    "url": "./static/css/main.4bcf4f67.chunk.css"
+  },
+  {
+    "revision": "0ce329b39c4c7d5e80a4",
+    "url": "./static/js/2.8d81c3b5.chunk.js"
+  },
+  {
+    "revision": "77ad6023570002ac00e6fc11e8c0c904",
+    "url": "./static/js/2.8d81c3b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6249a035b1e16a9a9e22",
+    "url": "./static/js/main.3e9a0acb.chunk.js"
+  },
+  {
+    "revision": "6401af9ca589de6236ed",
+    "url": "./static/js/runtime-main.463274a3.js"
+  },
+  {
+    "revision": "f8a39b5de8b2ec92df8d0ccc35703955",
+    "url": "./static/media/engineer.f8a39b5d.svg"
+  },
+  {
+    "revision": "fd5728f2cf777b06b966d05c0c823dc9",
+    "url": "./static/media/spritesheet.fd5728f2.svg"
+  }
+]);
